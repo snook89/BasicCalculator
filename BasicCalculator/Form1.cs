d03e7a26436204d292a675945902d74735c88858
snook89 <kgornostay@gmail.com>
2020-05-11 20:57:25 -0700

    Добавьте файлы проекта.

---
+++ BasicCalculator/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BasicCalculator
+{
+    /// <summary>
+    /// A basic calculator
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        #region Constructor
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Clearing Methods
+        private void CEButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DelButton_Click(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+
+        #region Number Methods
+
+        private void SevenButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EightButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NineButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FourButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FiveButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SixButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OneButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TwoButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ThreeButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ZerroButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+#endregion
+        
+        #region Operator Methods
+        private void DivideButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TimesButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MinusButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PlusButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DotButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EqualsButton_Click(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+    }
+}
